859cda68922551a8790406344841c7877941a9dc
weburetech <nitin@webure.in>
2021-04-03 16:53:24 +0530

    Rewy Theme without Strapi

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6d41af581bc9b0bc7ba37e0db0e5c37e'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b6e846aa-74f7-44b2-bb69-95066cf91db5'
-};
