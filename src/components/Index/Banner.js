c1d6c9c0f1cc3fba8a6502d22b4194baa959f343
weburetech <nitin@webure.in>
2021-04-16 00:50:17 +0530

    Update Banner.js

--- src/components/Index/Banner.js
+++ src/components/Index/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
                     <div className="col-lg-6 col-md-12">
                         <div className="main-banner-content">
                             <ReactWOW delay='.1s' animation='fadeInLeft'>
-                                <h1>Webure a<strong>Full Service Digital Agency</strong></h1>
+                                <h1>Webure a<strong> Full Service Digital Agency</strong></h1>
                             </ReactWOW>
 
                             <ReactWOW delay='.1s' animation='fadeInLeft'>
