834eed8b8eb6e9d68b62095464e2a6dd4a83d41a
weburetech <nitin@webure.in>
2021-04-16 00:43:32 +0530

    Update SEO.js

--- src/components/App/SEO.js
+++ src/components/App/SEO.js
@@ -10,7 +10,7 @@ const SEO = () => {
                 <meta name="description" content="Webure Technologies - Consult | Create | Collaborate" />
                 <meta name="og:title" property="og:title" content="Webure Technologies - Consult | Create | Collaborate"></meta>
                 <meta name="twitter:card" content="Webure Technologies - Consult | Create | Collaborate"></meta>
-                <link rel="canonical" href="https://weburemaster.gatsbyjs.io/"></link>
+                <link rel="canonical" href="https://webure.in/"></link>
                 <meta property="og:image" content="https://res.cloudinary.com/dev-empty/image/upload/v1593069801/explore-learning.jpg" />
             </Helmet>
         </div>
