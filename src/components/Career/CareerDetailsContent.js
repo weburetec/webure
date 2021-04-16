e1bb71f630583bb08b5e59d18fa8d61941157f81
weburetech <nitin@webure.in>
2021-04-17 03:26:06 +0530

    Update CareerDetailsContent.js

--- src/components/Career/CareerDetailsContent.js
+++ src/components/Career/CareerDetailsContent.js
@@ -41,31 +41,25 @@ const CareerDetailsContent = () => {
 								<a class="btn btn-green text-uppercase btn-medium rounded-3 w-180 mr-4 mb-5" href="/#">Apply to this job</a>
 								<a class="btn btn-outline-mercury text-black-2 text-uppercase h-px-48 rounded-3 mb-5 px-5" href="/#">
 								<i class="icon icon-bookmark-2 font-weight-bold mr-4 font-size-4"></i> Save job</a>
-							</div>
-						</div>	
-							<div class="job-details-content pt-8 pl-sm-9 pl-6 pr-sm-9 pr-6 pb-10 light-mode-texts">
-								<div class="row">
-									<div class="col-xl-11 col-md-12 pr-xxl-9 pr-xl-10 pr-lg-20">
-										<div class="">
-											<p class="mb-4 font-size-4 text-gray">Job Description</p>
-											<p class="font-size-4 text-black-2 mb-7">Gubagoo is a fast growing provider of messaging and commerce solutions for automotive dealers changing the future of how people find, buy and service their vehicles. </p>
-										</div>
-										<div class="">
-											<span class="font-size-4 font-weight-semibold text-black-2 mb-7">Your Role:</span>
-											<p class="font-size-4 text-black-2 mb-7">We’re looking for a passionate individual to design beautiful and functional products for our customers at Gubagoo. We move very fast and you will be expected to contribute to a cross-functional product development squad, that includes product managers and developers, to deliver the UX and UI for the team to bring to life. </p>
-											<p class="font-size-4 text-black-2 mb-7">We are serious about remote work. You can work for from anywhere. </p>
-											<span class="font-size-4 font-weight-semibold text-black-2 mb-7">What you will be doing:</span>
-												<ul class="list-unstyled">
-													<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-2"><span class="d-inline-block mr-7">•</span>Contribute new controls or design improvements to our</li>
-													<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-1"><span class="d-inline-block mr-7">•</span>Take ownership of the successful delivery of features</li>
-													<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-1"><span class="d-inline-block mr-7">•</span>Help set and achieve quarterly goals</li>
-													<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-1"><span class="d-inline-block mr-7">•</span>Ship a TON of product improvements and features</li>
-												</ul>
-												<a class="btn btn-green text-uppercase btn-medium w-180 h-px-48 rounded-3 mr-4 mt-6" href="/#">Apply to this job</a>
-										</div>
-									</div>
+								<p class="mb-4 font-size-4 text-gray">Job Description</p>
+								<p class="font-size-4 text-black-2 mb-7">Gubagoo is a fast growing provider of messaging and commerce solutions for automotive dealers changing the future of how people find, buy and service their vehicles. </p>
+								<span class="font-size-4 font-weight-semibold text-black-2 mb-7">Your Role:</span>
+								<p class="font-size-4 text-black-2 mb-7">We’re looking for a passionate individual to design beautiful and functional products for our customers at Gubagoo. We move very fast and you will be expected to contribute to a cross-functional product development squad, that includes product managers and developers, to deliver the UX and UI for the team to bring to life. </p>
+								<p class="font-size-4 text-black-2 mb-7">We are serious about remote work. You can work for from anywhere. </p>
+								<span class="font-size-4 font-weight-semibold text-black-2 mb-7">What you will be doing:</span>								
+									<ul class="list-unstyled">
+										<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-2"><span class="d-inline-block mr-7">•</span>Contribute new controls or design improvements to our</li>
+										<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-1"><span class="d-inline-block mr-7">•</span>Take ownership of the successful delivery of features</li>
+										<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-1"><span class="d-inline-block mr-7">•</span>Help set and achieve quarterly goals</li>
+										<li class="d-block font-size-4 text-black-2 d-flex flex-row mt-1"><span class="d-inline-block mr-7">•</span>Ship a TON of product improvements and features</li>
+									</ul>
+								<div className="btn-box">
+									<Link to="#" className="default-btn">
+										<i className="flaticon-user"></i>APPLY NOW! <span></span>
+									</Link>
 								</div>
-							</div>						
+							</div>
+						</div>							
 					</div>
 					<div className="col-lg-4 col-md-12">
 						<CareerSidebar />
