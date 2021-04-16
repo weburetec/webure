303ef123ab1bff016fdd7b3a70042d71b24178b8
weburetech <nitin@webure.in>
2021-04-17 02:54:01 +0530

    Update CareerDetailsContent.js

--- src/components/Career/CareerDetailsContent.js
+++ src/components/Career/CareerDetailsContent.js
@@ -133,11 +133,10 @@ const CareerDetailsContent = () => {
 						</div>
 					</div>
 				</div>
-			</div>
-
             <div className="col-lg-4 col-md-12">
 				<CareerSidebar />
             </div>
+			</div>
         </section>
     )
 }
