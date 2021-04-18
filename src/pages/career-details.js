e1414adb8eefb5557673352b8d3fd3e366f207dd
weburetech <nitin@webure.in>
2021-04-18 15:35:20 +0530

    Update career-details.js

--- src/pages/career-details.js
+++ src/pages/career-details.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from "../components/App/Layout"
 import Navbar from "../components/App/Navbar"
-import PageBanner from '../components/Common/PageBanner'
 import Footer from "../components/App/Footer"
 import CareerDetailsContent from '../components/Career/CareerDetailsContent
 
