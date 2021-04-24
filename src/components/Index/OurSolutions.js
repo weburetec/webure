ca57e91584c18c126d5aa643607388adde73f091
weburetech <nitin@webure.in>
2021-04-24 23:41:33 +0530

    Update OurSolutions.js

--- src/components/Index/OurSolutions.js
+++ src/components/Index/OurSolutions.js
@@ -19,47 +19,55 @@ const OurSolutions = () => {
     return (
         <section className="solutions-area pb-70">
             <div className="container">
-				<div className="col-lg-6 col-md-12">
-                    <div className="about-image">
-						<img src={aboutImage} alt="banner" />
+                <div className="row align-items-center">
+                    <div className="col-lg-6 col-md-12">
+                        <div className="about-image">
+                            <img src={aboutImage} alt="banner" />
+                        </div>
                     </div>
-                </div>
-					
-		        <div className="col-lg-6 col-md-12">
-                    <div className="about-content">
-                        <div className="content">
-                            <h2>Drive Digital Revolution Through Data Science</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
-                            <ul className="features-list">
-                                <li>
-                                    <img src={icon4} alt="banner" />
-                                    <h3>10 Years</h3>
-                                    <p>On the market</p>
-                                </li>
-                                <li>
-                                    <img src={icon5} alt="banner" />
-                                    <h3>45+</h3>
-                                    <p>Team members</p>
-                                </li>
-                                <li>
-                                    <img src={icon6} alt="banner" />
-                                    <h3>100%</h3>
-                                    <p>Satisfaction rate</p>
-                                </li>
-                                <li>
-                                    <img src={icon7} alt="banner" />
-                                    <h3>80%</h3>
-                                    <p>Senior scientist</p>
-                                </li>
-                            </ul>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
-                            <Link to="/about-us" className="default-btn">
-                                <i className="flaticon-right"></i>More About Us<span></span>
-                            </Link>
+
+                    <div className="col-lg-6 col-md-12">
+                        <div className="about-content">
+                            <div className="content">
+                                <span className="sub-title">
+                                    <img src={starIcon} alt="banner" /> 
+                                    About Us
+                                </span>
+                                <h2>Drive Digital Revolution Through Data Science</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                
+                                <ul className="features-list">
+                                    <li>
+                                        <img src={icon4} alt="banner" />
+                                        <h3>10 Years</h3>
+                                        <p>On the market</p>
+                                    </li>
+                                    <li>
+                                        <img src={icon5} alt="banner" />
+                                        <h3>45+</h3>
+                                        <p>Team members</p>
+                                    </li>
+                                    <li>
+                                        <img src={icon6} alt="banner" />
+                                        <h3>100%</h3>
+                                        <p>Satisfaction rate</p>
+                                    </li>
+                                    <li>
+                                        <img src={icon7} alt="banner" />
+                                        <h3>80%</h3>
+                                        <p>Senior scientist</p>
+                                    </li>
+                                </ul>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                
+                                <Link to="/about-us" className="default-btn">
+                                    <i className="flaticon-right"></i>More About Us<span></span>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-			
+					
 			    <div className="section-title">
                     <span className="sub-title">
                         Our Solutions
