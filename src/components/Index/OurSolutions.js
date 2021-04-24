03f1f1755b351b0a02c7112ddc905747196d0d64
weburetech <nitin@webure.in>
2021-04-24 22:16:04 +0530

    Update OurSolutions.js

--- src/components/Index/OurSolutions.js
+++ src/components/Index/OurSolutions.js
@@ -10,13 +10,62 @@ import serviceb6 from '../../assets/images/b6.webp'
 import serviceb7 from '../../assets/images/b7.webp'
 import serviceb8 from '../../assets/images/b8.webp'
 
+import aboutImage from '../../assets/images/about/about-img5.webp'
+import starIcon from '../../assets/images/star-icon.webp'
+import icon4 from '../../assets/images/icons/icon4.webp'
+import icon5 from '../../assets/images/icons/icon5.webp'
+import icon6 from '../../assets/images/icons/icon6.webp'
+import icon7 from '../../assets/images/icons/icon7.webp'
+import shape1 from '../../assets/images/shape/circle-shape1.webp'
+
 const OurSolutions = () => {
     return (
         <section className="solutions-area pb-70">
             <div className="container">
+		        <div className="col-lg-6 col-md-12">
+                        <div className="about-content">
+                            <div className="content">
+                                <span className="sub-title">
+                                    <img src={starIcon} alt="banner" /> 
+                                    About Us
+                                </span>
+                                <h2>Drive Digital Revolution Through Data Science</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                
+                                <ul className="features-list">
+                                    <li>
+                                        <img src={icon4} alt="banner" />
+                                        <h3>10 Years</h3>
+                                        <p>On the market</p>
+                                    </li>
+                                    <li>
+                                        <img src={icon5} alt="banner" />
+                                        <h3>45+</h3>
+                                        <p>Team members</p>
+                                    </li>
+                                    <li>
+                                        <img src={icon6} alt="banner" />
+                                        <h3>100%</h3>
+                                        <p>Satisfaction rate</p>
+                                    </li>
+                                    <li>
+                                        <img src={icon7} alt="banner" />
+                                        <h3>80%</h3>
+                                        <p>Senior scientist</p>
+                                    </li>
+                                </ul>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                
+                                <Link to="/about-us" className="default-btn">
+                                    <i className="flaticon-right"></i>More About Us<span></span>
+                                </Link>
+                            </div>
+                        </div>
+                </div>
+			
+			
 			    <div className="section-title">
                     <span className="sub-title">
-                        <img src={starIcon} alt="star" class="lazyload"/> 
                         Our Solutions
                     </span>
                     <h2>Ready to Grow Business?</h2>
