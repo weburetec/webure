120d8b3f1685b8c26fa0000a003dc0ca25546634
weburetech <nitin@webure.in>
2021-04-29 23:42:50 +0530

    Update OurSolutions.js

--- src/components/Index/OurSolutions.js
+++ src/components/Index/OurSolutions.js
@@ -199,65 +199,206 @@ const OurSolutions = () => {
                     </div>
                 </div>
             </div>
-        </section>
-
-		<section class="home-contact elements--ellipse">
 			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-12">
-						<h2 class="sl--heading sl--darkGrey sl--marginT100 text-center aos-init aos-animate" data-aos-easing="ease" data-aos="fade-up" data-aos-offset="0" data-aos-delay="500">Identify potential buyers in your market</h2>
-					</div>
-				</div>
-				<div class="row flex-column-reverse flex-lg-row st-150">
-					<div class="col-lg-4 col-md-12 order-md-12 order-2">
-						<div class="media contact-media">
-							<img class="" src="https://d3ml3b6vywsj0z.cloudfront.net/website/v2/home/contact_group_1.svg" alt="Competitor Intelligence">
-							<div class="media-body">
-								<h5 class="sl-header">Competitor Intelligence</h5>
-								<ul class="contact-list list-unstyled">
-									<li>Market Share</li>
-									<li>New/Expiring Contracts</li>
-								</ul>
-							</div>
-						</div>
-						<div class="media contact-media">
-							<img class="" src="https://d3ml3b6vywsj0z.cloudfront.net/website/v2/home/contact_group_2.svg" alt="Buying Patterns">
-							<div class="media-body">
-								<h5 class="sl-header">Buying Patterns</h5>
-								<ul class="contact-list list-unstyled">
-									<li>Technology Stacks</li>
-									<li>Upcoming Renewals</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-12 order-md-12 order-3">
-						<div class="col-lg-4 col-md-12 order-md-12 order-1">
-							<div class="media contact-media">
-								<img class="" src="https://d3ml3b6vywsj0z.cloudfront.net/website/v2/home/contact_group_3.svg" alt="Buyers personas"/>
-								<div class="media-body">
-									<h5 class="sl-header">Buyers personas</h5>
-									<ul class="contact-list list-unstyled">
-										<li>Contact Information</li>
-										<li>Detailed Profiles</li>
-									</ul>
-								</div>
-							</div>
-							<div class="media contact-media">
-								<img class="" src="https://d3ml3b6vywsj0z.cloudfront.net/website/v2/home/contact_group_4.svg" alt="Market Intelligence"/>
-								<div class="media-body">
-									<h5 class="sl-header">Market Intelligence</h5>
-									<ul class="contact-list list-unstyled">
-										<li>Funding and Financial Alerts</li>
-										<li>Keyword Tracking</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+<div class="row">
+<div class="col-12">
+<h2 class="sl--heading sl--darkGrey aos-init aos-animate" data-aos-easing="ease" data-aos="fade-up">Explore technologies and companies</h2>
+<p class="sl-subTitle sl--lightGrey sl-marginT20 aos-init aos-animate" data-aos-easing="ease" data-aos="fade-up">Gain
+insights about products, services, partners, and competitors.</p>
+</div>
+<div class="col-12  elements--plus">
+<div class="discover__inputWrap search_bar">
+<div class="discover__input">
+<input class="" id="autoguggestion-input" placeholder="Search for technologies and companies by name or domain">
+<span class="search__icon"></span>
+<div id="embed_search_autosuggestion" class="company_autosuggestion unified_search_autosuggestion" style="display: none;">
+<div class="suggestion_block">
+<ul id="technology_autosuggestion">
+</ul>
+<ul id="company_autosuggestion">
+</ul>
+</div>
+<div class="embed_result_block">
+<p class="text-primary">See all results</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="row sl-marginT80">
+<div class="col-lg-6 col-md-12">
+<div class="horizontal-line"></div>
+<h2 class="discover__techText">Technologies</h2>
+<div class="row discover__techWrap sl--marginR20">
+<div class="col-md-6 col-sm-12">
+<a href="/tech/payment-management/stripe-market-share">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://ga0.imgix.net/logo/o/91015-1485358248-1568973?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format" alt="Generic placeholder image" onerror="this.onerror=null;this.src='https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png';" src="https://ga0.imgix.net/logo/o/91015-1485358248-1568973?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format">
+<div class="media-body">
+<h5 class="mt-0">Stripe</h5>
+<p>Finance and Accounting-Payment Management</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/tech/project-collaboration/slack-market-share">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://ga0.imgix.net/logo/o/91563-1523890522-2133787?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format" alt="Generic placeholder image" onerror="this.onerror=null;this.src='https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png';" src="https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png">
+<div class="media-body">
+<h5 class="mt-0">Slack</h5>
+<p>Collaboration-Project Collaboration</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/tech/analytics/segment-market-share">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://ga0.imgix.net/logo/o/101273-1497950693-535848?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format" alt="Generic placeholder image" onerror="this.onerror=null;this.src='https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png';" src="https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png">
+<div class="media-body">
+<h5 class="mt-0">Segment</h5>
+<p>Business Intelligence &amp; Analytics-Analytics</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/tech/online-meetings/zoom-market-share">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://ga1.imgix.net/logo/o/111633-1515416413-259357?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format" alt="Generic placeholder image" onerror="this.onerror=null;this.src='https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png';" src="https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png">
+<div class="media-body">
+<h5 class="mt-0">Zoom</h5>
+<p>Communications-Online Meetings</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/tech/contact-management/salesforce-crm-market-share">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://ga1.imgix.net/logo/o/90378-1427725601-6725419?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format" alt="Generic placeholder image" onerror="this.onerror=null;this.src='https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png';" src="https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png">
+<div class="media-body">
+<h5 class="mt-0">Salesforce CRM</h5>
+<p>Sales-Contact Management</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/tech/marketing-automation/marketo-market-share">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://ga0.imgix.net/logo/o/990-1500306114-455972?ixlib=rb-1.0.0&amp;ch=Width%2CDPR&amp;auto=format" alt="Generic placeholder image" onerror="this.onerror=null;this.src='https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png';" src="https://d3ml3b6vywsj0z.cloudfront.net/website/technology_icon.png">
+<div class="media-body">
+<h5 class="mt-0">Marketo</h5>
+<p>Marketing-Marketing Automation</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-6 col-md-12">
+<div class="horizontal-line"></div>
+<h2 class="discover__techText">Companies</h2>
+<div class="row discover__techWrap">
+<div class="col-md-6 col-sm-12">
+<a href="/company/5b89165a7c866675e510c378/coursera">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1502210913/rlanonwmo1mgrwuvacv9.png" alt="Generic placeholder image" src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1502210913/rlanonwmo1mgrwuvacv9.png">
+<div class="media-body">
+<h5 class="mt-0">Coursera</h5>
+<p>coursera.org</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/company/5b8915857c866675e50ff26e/juniper-networks">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1397190585/a258e2c803afdf25787ab01d53744ac6.gif" alt="Generic placeholder image" src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1397190585/a258e2c803afdf25787ab01d53744ac6.gif">
+<div class="media-body">
+<h5 class="mt-0">Juniper Networks</h5>
+<p>juniper.net</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/company/5b8916f97c866675e51166c3/lending-club">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/fnyd5qggryugrcynulkf" alt="Generic placeholder image" src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/fnyd5qggryugrcynulkf">
+<div class="media-body">
+<h5 class="mt-0">Lending Club</h5>
+<p>lendingclub.com</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/company/5c3b00a4d55ae49f1b76bfe9/twitter">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1397180207/d242197edc3ff044620cf2d8ff39d6b3.jpg" alt="Generic placeholder image" src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1397180207/d242197edc3ff044620cf2d8ff39d6b3.jpg">
+<div class="media-body">
+<h5 class="mt-0">Twitter</h5>
+<p>twitter.com</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/company/5b8915707c866675e50fdbec/xerox">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+<div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1397188986/a79a667532e561f2e9ab5f0d999241a9.png" alt="Generic placeholder image" src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/v1397188986/a79a667532e561f2e9ab5f0d999241a9.png">
+<div class="media-body">
+<h5 class="mt-0">Xerox</h5>
+<p>xerox.com</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-6 col-sm-12">
+<a href="/company/5c3b0155d55ae49f1b77b308/freshworks">
+<div class="discover__tech aos-init aos-animate" data-aos="fade-up" data-aos-offset="0" data-aos-easing="ease" data-aos-delay="1000">
+ <div class="media">
+<img class="align-self-center mr-3 discover__techImg lazyloaded" data-src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/lmvkrk1etrborilzp8yb" alt="Generic placeholder image" src="https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_jpg/lmvkrk1etrborilzp8yb">
+<div class="media-body">
+<h5 class="mt-0">Freshworks</h5>
+<p>freshworks.com</p>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+			
+        </section>
 
     )
 }
