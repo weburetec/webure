8633169cf16edc51ec60487671a6708ebf23ce90
weburetech <nitin@webure.in>
2021-04-30 03:27:08 +0530

    Updates

---
+++ src/components/ServiceDetails/CareerSidebar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+const CareerSidebar = () => {
+    return (
+        <div className="events-details-info">
+            <ul className="info">
+                <li>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <span>JOB ID</span>
+                        WT/BRLHYD/DEV/001
+                    </div>
+                </li>
+                <li>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <span>Department</span>
+                        Development
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default CareerSidebar
---
+++ src/components/ServiceDetails/ContactForm.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { useState } from "react";
+import starIcon from '../../assets/images/star-icon.webp'
+import contact from '../../assets/images/contact.webp'
+
+const ContactForm = () => {
+	const [status, setStatus] = useState("Submit");
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setStatus("Sending...");
+		const { name, email, message } = e.target.elements;
+			let details = {
+				name: name.value,
+				email: email.value,
+				message: message.value,
+			};
+			let response = await fetch("http://localhost:5000/contact", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json;charset=utf-8",
+				},
+				body: JSON.stringify(details),
+			});
+		setStatus("Submit");
+		let result = await response.json();
+		alert(result.status);
+	};
+    return (
+        <section className="contact-area pb-100">
+            <div className="container">
+                <div className="section-title">
+                    <h4>Apply Job</h4>
+                </div>
+
+                <div className="row">
+                        <div className="contact-form">
+                            <form id="contactForm">
+                                <div className="row">
+                                    <div className="col-lg-12 col-md-6">
+                                        <div className="form-group">
+                                            <input type="text" name="name" className="form-control" required placeholder="Name" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-6">
+                                        <div className="form-group">
+                                            <input type="email" name="email" className="form-control" required placeholder="Your email address" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <input type="text" name="phone_number" className="form-control" required placeholder="Your phone number" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Objective" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Experience" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="- To make money. To kill time. To get popular. Sorry, not interested. Next!  To learn and learn more. To take up a challenging role. To grow with growing organization. Great!!!" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Write your message..." />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <button type="submit" className="default-btn">
+                                            <i className="flaticon-tick"></i> 
+                                            Send Message <span></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ContactForm
