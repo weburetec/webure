dd08e146d5924cd9b1fcf4ba93b74dd9467072aa
weburetech <nitin@webure.in>
2021-05-01 01:24:05 +0530

    Update ServiceDetailsContent.js

--- src/components/ServiceDetails/ServiceDetailsContent.js
+++ src/components/ServiceDetails/ServiceDetailsContent.js
@@ -36,21 +36,21 @@ const CareerDetailsContent = () => {
 							<div className="col-lg-8 col-md-12">
 								<div class="services-box">
 									<div class="service-warp wrap1">
-										<a href="https://www.dimakhconsultants.com/digital-marketing/google-partners-certified-company-pune" class="service service1 bg-blue"/>
+										<a href="https://www.dimakhconsultants.com/digital-marketing/google-partners-certified-company-pune" class="service service1 bg-blue">
 											<img src="https://www.dimakhconsultants.com/img/digital-marketing/Google-partner.png" alt=""/>
 											<h5>Google Partner</h5>
 										</a>
-										<a href="https://www.dimakhconsultants.com/digital-marketing/seo-marketing-company-pune-india" class="service service2 bg-limegreen"/>
+										<a href="https://www.dimakhconsultants.com/digital-marketing/seo-marketing-company-pune-india" class="service service2 bg-limegreen">
 											<img src="https://www.dimakhconsultants.com/img/digital-marketing/Search-engine-optimization.png" alt=""/>
 											<h5>Search Engine Optimization</h5>
 										</a>
 									</div>
 									<div class="service-wrap wrap2">
-										<a href="https://www.dimakhconsultants.com/digital-marketing/conversion-rate-optimisation-company-pune-india" class="service service3 bg-skyblue"/>
+										<a href="https://www.dimakhconsultants.com/digital-marketing/conversion-rate-optimisation-company-pune-india" class="service service3 bg-skyblue">
 											<img src="https://www.dimakhconsultants.com/img/digital-marketing/Conversion-rate-optimization.png" alt=""/>
 											<h5>Conversion Rate Optimization </h5>
 										</a>
-										<a href="https://www.dimakhconsultants.com/digital-marketing/ppc-marketing-company-pune-india" class="service service4 bg-yellow"/>
+										<a href="https://www.dimakhconsultants.com/digital-marketing/ppc-marketing-company-pune-india" class="service service4 bg-yellow">
 											<img src="https://www.dimakhconsultants.com/img/digital-marketing/PPC-marketing.png" alt=""/>
 											<h5>PPC Marketing </h5>
 										</a>
