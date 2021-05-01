4f16f04e2f501d36bbf6526150bbd640f583ac2c
weburetech <nitin@webure.in>
2021-05-02 00:45:52 +0530

    Updates

--- src/components/ServiceDetails/ServiceDetailsContent.js
+++ src/components/ServiceDetails/ServiceDetailsContent.js
@@ -96,6 +96,38 @@ const CareerDetailsContent = () => {
 								<p>Webure Technologies New York brings effective tools for making your website popular on the Internet with the help of a planned strategy for your web marketing efforts. Effective Web Marketing is a combination of many important aspects of the Web and Internet techniques. </p>
 							<br/><br/>
 							</div>
+							
+							<div class="row">
+								<div class="card-container">
+									<span class="pro">PRO</span>
+									<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+									<h3>Ricky Park</h3>
+									<h6>New York</h6>
+									<p>User interface designer and <br/> front-end developer</p>
+									<div class="buttons">
+										<button class="primary">
+											Message
+										</button>
+										<button class="primary ghost">
+											Following
+										</button>
+									</div>
+									<div class="skills">
+										<h6>Skills</h6>
+										<ul>
+											<li>UI / UX</li>
+											<li>Front End Development</li>
+											<li>HTML</li>
+											<li>CSS</li>
+											<li>JavaScript</li>
+											<li>React</li>
+											<li>Node</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							
 							<div class="row">
 								<div class="col-lg-6">
 									<div class="achievement-col col_1 mr-lg-5 mb-5">
