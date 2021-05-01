da14e1e70598b9c751027ccbe110dd00cc94657b
weburetech <nitin@webure.in>
2021-05-02 02:47:53 +0530

    Update ServiceDetailsContent.js

--- src/components/ServiceDetails/ServiceDetailsContent.js
+++ src/components/ServiceDetails/ServiceDetailsContent.js
@@ -99,44 +99,43 @@ const CareerDetailsContent = () => {
 
 							<div class="row">
 								<div class="col-md-6">
-
-<div class="first hero">
-  <img class="hero-profile-img" src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg" alt=""/>
-  <div class="hero-description-bk"></div>
-  <div class="hero-logo">
-    <img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
-  </div>
-  <div class="hero-description">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-  <div  class="hero-date">
-    <p>20.02.2019</p>
-  </div>
-  <div class="hero-btn">
-    <a href="#">Learn More</a>
-  </div>
-</div>
-
-<div class="second hero">
-  <img class="hero-profile-img" src="https://i.pinimg.com/originals/9f/b1/ff/9fb1ffb05dd28039d1a06ad8e90cbd75.jpg" alt=""/>
-  <div class="hero-description-bk"></div>
-  <div class="hero-logo">
-    <img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
-  </div>
-  <div class="hero-description">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-  <div  class="hero-date">
-    <p>20.02.2019</p>
-  </div>
-  <div class="hero-btn">
-    <a href="#">Learn More</a>
-  </div>
-</div>
+									<div class="first hero">
+									  <img class="hero-profile-img" src="https://cdn.gamer-network.net/2018/metabomb/leagueoflegendsbestcarrychampions2018pantheon.jpg" alt=""/>
+									  <div class="hero-description-bk"></div>
+									  <div class="hero-logo">
+										<img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
+									  </div>
+									  <div class="hero-description">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+									  </div>
+									  <div  class="hero-date">
+										<p>20.02.2019</p>
+									  </div>
+									  <div class="hero-btn">
+										<a href="#">Learn More</a>
+									  </div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="second hero">
+									  <img class="hero-profile-img" src="https://i.pinimg.com/originals/9f/b1/ff/9fb1ffb05dd28039d1a06ad8e90cbd75.jpg" alt=""/>
+									  <div class="hero-description-bk"></div>
+									  <div class="hero-logo">
+										<img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
+									  </div>
+									  <div class="hero-description">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+									  </div>
+									  <div  class="hero-date">
+										<p>20.02.2019</p>
+									  </div>
+									  <div class="hero-btn">
+										<a href="#">Learn More</a>
+									  </div>
+									</div>
 								</div>
 							</div>
-							
-							
+														
 							<div class="row">
 								<div class="col-md-6">
 									<div class="card-container">
