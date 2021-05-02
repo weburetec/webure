756806a3fabbdc02cb17ad56ae6c3c99c2912cde
weburetech <nitin@webure.in>
2021-05-03 00:05:48 +0530

    Updates

--- src/components/ServiceDetails/ServiceDetailsContent.js
+++ src/components/ServiceDetails/ServiceDetailsContent.js
@@ -99,7 +99,7 @@ const CareerDetailsContent = () => {
 														
 							<div class="row">
 								<div class="col-md-6">
-									<div class="card-container">
+									<div class="card-container achievement-col col_1">
 										<span class="pro">PRO</span>
 										<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
 										<h3>Ricky Park</h3>
@@ -129,7 +129,37 @@ const CareerDetailsContent = () => {
 								</div>
 								
 								<div class="col-md-6">
-									<div class="card-container">
+									<div class="card-container achievement-col col_2">
+										<span class="pro">PRO</span>
+										<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+										<h3>Ricky Park</h3>
+										<h6>New York</h6>
+										<p>User interface designer and <br/> front-end developer</p>
+										<div class="buttons">
+											<button class="primary">
+												Message
+											</button>
+											<button class="primary ghost">
+												Following
+											</button>
+										</div>
+										<div class="skills">
+											<h6>Skills</h6>
+											<ul>
+												<li>UI / UX</li>
+												<li>Front End Development</li>
+												<li>HTML</li>
+												<li>CSS</li>
+												<li>JavaScript</li>
+												<li>React</li>
+												<li>Node</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-md-6">
+									<div class="card-container achievement-col col_3">
 										<span class="pro">PRO</span>
 										<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
 										<h3>Ricky Park</h3>
