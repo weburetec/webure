31f958bcc71579b759c0caaaa46fd5707747beae
weburetech <nitin@webure.in>
2021-05-05 00:11:33 +0530

    Update DedicatedServerContent.js

--- src/components/ServiceDetails/DedicatedServerContent.js
+++ src/components/ServiceDetails/DedicatedServerContent.js
@@ -29,17 +29,7 @@ const CareerDetailsContent = () => {
 								<span class="font-size-8 mb-0">Do you have an idea for an app and are looking for a trusted app design and development agency in India ? Look no further ! The experienced digital marketing professionals at Webure Technologies work with you to develop a media strategy, conduct media research, create a media plan and ultimately help you to launch and optimize your online digital campaign. Webure Technologies offers complete Digital Marketing package including web design , website development, Internet marketing. Our range of internet marketing services includes search engine optimization (SEO), Google Ads, email marketing solution etc.</span>						
 							</div>						
 						</div>
-						<br/><br/>
-						
-						
-						<div class="row">
-							<div class="col-12">
-								<h6 class="sec-head">Why Webure Technologies</h6>
-								<p>We are a full service web based interactive marketing agency that develops world-class websites and online internet campaigns (Google Adwords). Our experts work in harmony to strategically integrate cutting-edge technology solutions with a sensible Internet marketing approach, providing superior brand and user experiences that deliver results to our global clients.</p>
-								<p>Webure Technologies New York brings effective tools for making your website popular on the Internet with the help of a planned strategy for your web marketing efforts. Effective Web Marketing is a combination of many important aspects of the Web and Internet techniques. </p>
-							<br/><br/>
-							</div>						
-						</div>			
+						<br/><br/>			
 					</div>
 					<div className="col-lg-4 col-md-12">
 						<img src={bannerhex} alt="bannerhex" />
@@ -49,132 +39,62 @@ const CareerDetailsContent = () => {
 						</div>
 					</div>
 				</div>
+				
 				<div class="row">
-					<div class="col-lg-4">
-						<div class="card-container achievement-col col_1">
-							<span class="pro">PRO</span>
-							<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
-							<h3>Ricky Park</h3>
-							<h6>New York</h6>
-							<p>User interface designer and <br/> front-end developer. Support Covid - successful implementation of COVID KAVACH An award winning app for clinical trial participants symptoms tracking</p>
-							<div class="buttons">
-								<button class="primary">
-									Message
-								</button>
-								<button class="primary ghost">
-									Following
-								</button>
-							</div>
-							<div class="skills">
-								<h6>Skills</h6>
-								<ul>
-									<li>UI / UX</li>
-									<li>Front End Development</li>
-									<li>HTML</li>
-									<li>CSS</li>
-									<li>JavaScript</li>
-									<li>React</li>
-									<li>Node</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-								
-					<div class="col-lg-4">
-						<div class="card-container achievement-col col_1">
-							<span class="pro">PRO</span>
-							<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
-							<h3>Ricky Park</h3>
-							<h6>New York</h6>
-							<p>User interface designer and <br/> front-end developer. Support Covid - successful implementation of COVID KAVACH An award winning app for clinical trial participants symptoms tracking</p>
-							<div class="buttons">
-								<button class="primary">
-									Message
-								</button>
-								<button class="primary ghost">
-									Following
-								</button>
-							</div>
-							<div class="skills">
-								<h6>Skills</h6>
-								<ul>
-									<li>UI / UX</li>
-									<li>Front End Development</li>
-									<li>HTML</li>
-									<li>CSS</li>
-									<li>JavaScript</li>
-									<li>React</li>
-									<li>Node</li>
-								</ul>
-							</div>
-						</div>
-					</div>					
-
-					<div class="col-lg-4">
-						<div class="card-container achievement-col col_1">
-							<span class="pro">PRO</span>
-							<img class="round" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
-							<h3>Ricky Park</h3>
-							<h6>New York</h6>
-							<p>User interface designer and <br/> front-end developer. Support Covid - successful implementation of COVID KAVACH An award winning app for clinical trial participants symptoms tracking</p>
-							<div class="buttons">
-								<button class="primary">
-									Message
-								</button>
-								<button class="primary ghost">
-									Following
-								</button>
-							</div>
-							<div class="skills">
-								<h6>Skills</h6>
-								<ul>
-									<li>UI / UX</li>
-									<li>Front End Development</li>
-									<li>HTML</li>
-									<li>CSS</li>
-									<li>JavaScript</li>
-									<li>React</li>
-									<li>Node</li>
-								</ul>
-							</div>
-						</div>
-					</div>
+				
+				<div class="container table-responsive py-5"> 
+<table class="table table-bordered table-hover">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+     <tr>
+      <th scope="row">4</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+				
 				</div>
 				
 				<div class="row">
-					<div className="col-lg-2 col-md-12">
-						<br/><br/>
-						<div class="achievements-hex d-flex align-items-center justify-content-center font-color-white">
-							<h4><strong>Our Client</strong></h4>
-						</div>
-					</div>
-					<div className="col-lg-10 col-md-12">
-						<br/><br/>
-						<p class="pt-lg-4">Our Work, Projects and Portfolio speaks for itself. With our strong global delivery model, we serve clients worldwide across USA, UK, Europe, Singapore, Australia, Canada, Dubai - UAE, Sri Lanka. Our spectrum of services include graphic designs, Social Media Designs and websites to highly complex database driven applications, mobile apps, digital marketing projects and global server hosting. Dimakh Consultants can design and build an end-to-end digital solutions that will work for you and your clients.</p>
-					</div>
 					<div class="col-lg-1"></div>
 					<div class="col-lg-10">
 						<img src="https://tistio.com/wp-content/uploads/2020/11/map-1024x655.png" alt="DCPL Presence Map" title="DCPL Presence Map" class="img-fluid mt-4"/>
 					</div>
 					<div class="col-lg-1"></div>
-					<div class="row">
-						<div class="industries-serve text-center mt-5">
-							<h4>Industries We Serve:</h4>
-							<ul class="list-unstyled common-list">
-								<li> Automobiles</li>
-								<li> Banking</li>
-								<li> Financial Services</li>
-								<li> Infrastructure</li>
-								<li> IT &amp; ITES</li>
-								<li> Manufacturing</li>
-								<li> Pharmaceuticals</li>
-								<li> Power</li>
-								<li> Real Estate</li>
-								<li> Telecommunications</li>
-								<li> Textiles</li>
-							</ul>
-						</div>
-					</div>
 				</div>
 			</div>
         </section>
