69d65c7bec7db0f54ad2ab503a0cf9c83b0bc67f
weburetech <nitin@webure.in>
2021-05-05 01:48:17 +0530

    Update DedicatedServerContent.js

--- src/components/ServiceDetails/DedicatedServerContent.js
+++ src/components/ServiceDetails/DedicatedServerContent.js
@@ -45,253 +45,36 @@ const DedicatedServerContent = () => {
             <div className="membership-levels-area ptb-100">
                 <div className="container">
                     <div className="membership-levels-table table-responsive">
-                        <table className="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        <span className="price">$500</span>
-                                        <span className="title">Silver Membership</span>
-                                        <span className="desc">You can view few features</span>
-                                    </th>
-                                    <th>
-                                        <span className="price">$1,000</span>
-                                        <span className="title">Gold Membership</span>
-                                        <span className="desc">You can view most features</span>
-                                    </th>
-                                    <th>
-                                        <span className="price">$1,500</span>
-                                        <span className="title">Diamond Membership</span>
-                                        <span className="desc">You can view all features</span>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Number of features</td>
-                                    <td>4</td>
-                                    <td>8</td>
-                                    <td>15</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Investment Fundamentals & Data Analytics
-                                        </Link>
-                                    </td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Hands-On Python & R In Data Science
-                                        </Link>
-                                    </td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Complete Data Science Bootcamp
-                                        </Link>
-                                    </td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Programming MasterclassName for Developers
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Hands-On Artificial Neural Networks
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            PRINCE2® Practitioner Certification Training
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            PMI-RMP® Certification Training
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            PRINCE2® Foundation Certification Training
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            PMP® Plus
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            PMP® Renewal Pack
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Digital Project Manager
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Agile Project Management; Agile Delivery
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Vue JS 2 - The Complete Guide
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Everything You Need to Program in Python
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Mathematical Foundation For Machine Learning
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Drawing Course - Beginner to Advanced
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Deep Learning and Artificial Intelligence
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Information About UI/UX Design Degree
-                                        </Link>
-                                    </td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Photography Photo modify and Beautiful
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Link to="#">
-                                            Professional IT Expert Certificate Course
-                                        </Link>
-                                    </td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-none"><i className='bx bx-x'></i></td>
-                                    <td className="item-check"><i className='bx bx-check'></i></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td>
-                                        <Link to="#" className="select-btn">
-                                            Get it now
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <Link to="#" className="select-btn">
-                                            Get it now
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <Link to="#" className="select-btn">
-                                            Get it now
-                                        </Link>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+
+<table class="table dedicated-table">
+<thead>
+<tr>
+<th>&nbsp;</th>
+<th>Servers</th>
+<th>CPU Cores/Speed</th>
+<th>Memory</th>
+<th>Storage</th>
+<th>Bandwidth</th>
+<th>Deployment Time</th>
+<th>Monthly</th>
+<th>&nbsp;</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><img src="https://serverhub.com/themes/serverhub/assets/img/xeon.png"/> </td>
+<td>Intel Xeon E3</td>
+<td>Intel Xeon E3</td>
+<td>Intel Xeon E3</td>
+<td>Intel Xeon E3</td>
+<td>Intel Xeon E3</td>
+<td><b>24 Hours</b></td>
+<td><b>Intel Xeon E3</b></td>
+<td><a style="color:#85c342" href="https://my.serverhub.com/cart.php?a=add&amp;pid=625">Configure</a></td>
+</tr>
+</tbody>
+</table>
+
                     </div>
                 </div>
             </div>
