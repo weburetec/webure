e205d98eb68d0e239c127430d2fa565a75b547f5
weburetech <nitin@webure.in>
2021-05-06 01:37:11 +0530

    Update dedicatedserver.js

--- src/pages/dedicatedserver.js
+++ src/pages/dedicatedserver.js
@@ -15,8 +15,6 @@ const Details = () => {
 			<section class="pt-xl" data-scroll-target="addons" id="addons">
 				<div class="l-page container align-center">
 					<h2 class="align-center">Enhance your Freshchat experience</h2>
-					<p>&nbsp;</p>
-					<input type="hidden" class="product-key" data-product-key="fchat">
 					<div class="vertical-tab-wrapper vertical-tab-pricing-wrapper tablist-container">
 						<div class="row">
 							<div class="col-md-5">
@@ -26,22 +24,6 @@ const Details = () => {
 											<span class="pricing-pill-text">Freddy</span>
 											<span class="add-on-tab-heading">Additional bot sessions</span>
 											<span class="add-on-tab-desc"><i>Use chatbots with superior API connectors to automate the resolution of issues.</i></span>
-										</a>
-										<span class="down-arrow-rounded"></span>
-									</li>
-									<li role="presentation" class="">
-										<a href="#addon2" id="addon2-tab" class="" role="tab" data-toggle="tab" aria-controls="addon2" aria-expanded="false">
-											<span class="pricing-pill-text">Add-On</span>
-											<span class="add-on-tab-heading">WhatsApp Business</span>
-											<span class="add-on-tab-desc"><i>Leverage the power of WhatsApp. Bring all messages from WhatsApp to one Inbox to support and notify your customers.</i></span>
-										</a>
-										<span class="down-arrow-rounded"></span>
-									</li>
-									<li role="presentation" class="">
-										<a href="#addon3" id="addon3-tab" class="" role="tab" data-toggle="tab" aria-controls="addon3" aria-expanded="false">
-											<span class="pricing-pill-text">Add-On</span>
-											<span class="add-on-tab-heading">Acquisition &amp; Engagement</span>
-											<span class="add-on-tab-desc"><i>Acquire and engage customers using proactive campaigns, announcements, automated meeting scheduling and more</i></span>
 										</a>
 										<span class="down-arrow-rounded"></span>
 									</li>
