7e4c526a7725bf148c90105e61abe053884dffdc
weburetech <nitin@webure.in>
2021-05-08 01:44:15 +0530

    Update dedicatedserver.js

--- src/pages/dedicatedserver.js
+++ src/pages/dedicatedserver.js
@@ -82,183 +82,150 @@ const Details = () => {
 				</div>
 			</section>
 			
-			<section class="pt-xl  " data-scroll-target="" id="">
-			  <div class="l-page container">
-				<div class="l-section-heading">
-				  
-				  
-			  <h2>Frequently Asked Questions</h2>
-
-
-
-				</div>
-				<div class="row">
-				  <div class="col-md-6">
-					
-				  
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">How does the 21-day free trial work?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content" style="display: none;">
-					<div class="accordion-text">
-					  <p>When you sign up, you instantly get access to our fully-loaded Garden plan for 21&nbsp;days. We will alert you before your trial expires. You can then select a plan and pay by credit card to continue using the product. If you choose not to continue, your account will be suspended.</p>
-
+			<section class="pt-xl" data-scroll-target="" id="">
+				<div class="l-page container">
+					<div class="l-section-heading">
+						<h2>Frequently Asked Questions</h2>
 					</div>
-				  </div>   
+					<div class="row">
+						<div class="col-md-6">
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">How does the 21-day free trial work?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content" style="display: none;">
+									<div class="accordion-text">
+										<p>When you sign up, you instantly get access to our fully-loaded Garden plan for 21&nbsp;days. We will alert you before your trial expires. You can then select a plan and pay by credit card to continue using the product. If you choose not to continue, your account will be suspended.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">How does the 21-day free trial work?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content" style="display: none;">
+									<div class="accordion-text">
+										<p>When you sign up, you instantly get access to our fully-loaded Garden plan for 21&nbsp;days. We will alert you before your trial expires. You can then select a plan and pay by credit card to continue using the product. If you choose not to continue, your account will be suspended.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">What are contacts and how does pricing work for it?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>Contacts are leads and users you chat with. With Freshchat, get unlimited contacts on any plan — save and access their data without pay anything extra</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">How safe is my data?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>We take security seriously and you get to choose your data center from the get-go. Our servers are hosted in world-class data centers that are protected by biometric locks and 24-hour surveillance. We ensure that our application is always up to date with the latest security patches.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">Do you have any cancellation fees?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>No. Freshchat is a pay-as-you-go service. We do not have any cancellation fees. You can cancel whenever you want but your cancellation will come into effect only after the end of your term.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">What types of payment do you accept?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>We accept Visa, Mastercard, Discover and American Express. We do not accept PayPal. We will not be able to accept purchase orders over the phone. All of our offline payments follow USD pricing.</p>
+									</div>
+								</div>   
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">How does the 21-day free trial work?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content" style="display: none;">
+									<div class="accordion-text">
+										<p>When you sign up, you instantly get access to our fully-loaded Garden plan for 21&nbsp;days. We will alert you before your trial expires. You can then select a plan and pay by credit card to continue using the product. If you choose not to continue, your account will be suspended.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">How does the 21-day free trial work?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content" style="display: none;">
+									<div class="accordion-text">
+										<p>When you sign up, you instantly get access to our fully-loaded Garden plan for 21&nbsp;days. We will alert you before your trial expires. You can then select a plan and pay by credit card to continue using the product. If you choose not to continue, your account will be suspended.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">What are contacts and how does pricing work for it?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>Contacts are leads and users you chat with. With Freshchat, get unlimited contacts on any plan — save and access their data without pay anything extra</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">How safe is my data?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>We take security seriously and you get to choose your data center from the get-go. Our servers are hosted in world-class data centers that are protected by biometric locks and 24-hour surveillance. We ensure that our application is always up to date with the latest security patches.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">Do you have any cancellation fees?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>No. Freshchat is a pay-as-you-go service. We do not have any cancellation fees. You can cancel whenever you want but your cancellation will come into effect only after the end of your term.</p>
+									</div>
+								</div>   
+							</div>
+							<div class="accordion-wrapper accordion-85">
+								<div class="accordion-float-wrapper">
+									<h3 class="accordion-heading">What types of payment do you accept?</h3>
+									<div class="accordion-expand"></div>
+								</div>
+								<div class="accordion-content">
+									<div class="accordion-text">
+										<p>We accept Visa, Mastercard, Discover and American Express. We do not accept PayPal. We will not be able to accept purchase orders over the phone. All of our offline payments follow USD pricing.</p>
+									</div>
+								</div>   
+							</div>
+						</div>
+					</div> <!-- End row -->
 				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">Can I upgrade or downgrade my subscription?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>Yes, you can choose to upgrade your subscription instantly. If you wish to downgrade or cancel your subscription, you can do so at the end of your term. Click on ‘Billing’ in your Freshchat account to modify your subscription details.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">What are contacts and how does pricing work for it?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>Contacts are leads and users you chat with. With Freshchat, get unlimited contacts on any plan — save and access their data without pay anything extra</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">How safe is my data?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>We take security seriously and you get to choose your data center from the get-go. Our servers are hosted in world-class data centers that are protected by biometric locks and 24-hour surveillance. We ensure that our application is always up to date with the latest security patches.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">Do you have any cancellation fees?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>No. Freshchat is a pay-as-you-go service. We do not have any cancellation fees. You can cancel whenever you want but your cancellation will come into effect only after the end of your term.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">What types of payment do you accept?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>We accept Visa, Mastercard, Discover and American Express. We do not accept PayPal. We will not be able to accept purchase orders over the phone. All of our offline payments follow USD pricing.</p>
-
-					</div>
-				  </div>   
-				</div>
-
-
-				  </div>
-				  <div class="col-md-6">
-					
-				  
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">What is MUV and how does it work?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>MUV stands for Monthly Unique Visitor. A monthly unique visitor is an individual user who accesses your site within a 30-day window.&nbsp;For mobile apps, the equivalent is a monthly active user - an app user who opened your app at least once within the 30-day window.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">Is WhatsApp Business add-on charged per user?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>No, the WhatsApp for Business integration is a flat monthly fee per WhatsApp number, regardless of the number of agents. You can find more details about this <a href="https://www.freshworks.com/live-chat-software/integrations/whatsapp/?source=pricing">here</a>.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">What does Omnichannel include?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>Each agent on Omnichannel has access to all features from Freshchat, Freshcaller, and Freshdesk, to support customers across channels from one place, with all the context and customer information. Omnichannel plans will include the Estate and Forest plans of Freshdesk, Freshchat, and Freshcaller respectively, with over 40% discount.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">What is Freddy?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>Freddy is a premium AI offering from Freshworks. It includes AI bots for stellar self-service and agent-assist AI features that automate mundane tasks for agents to improve their productivity. To know more about Freddy, <a href="https://www.freshworks.com/freddy-ai/">click here.</a></p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">What is a bot session?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>A bot session is any unique interaction between an end-user and a bot:</p>
-
-			<ul class="list-fchat">
-				<li>On email, every email response is counted as 1 bot session</li>
-				<li>On voice, every voice bot response is counted as 1 bot session</li>
-				<li>On chat, all end-users to bot interactions in a day are counted as 1 bot session and lasts for a maximum of 24 hours.&nbsp;</li>
-			</ul>
-
-			<p>Only customer-facing AI bot features consume bot sessions while agent-facing AI features and bots don’t consume bot sessions.</p>
-
-					</div>
-				  </div>   
-				</div>
-				<div class="accordion-wrapper accordion-85">
-				  <div class="accordion-float-wrapper">
-					<h3 class="accordion-heading">How can I buy additional bot sessions? What's the validity of a pack?</h3>
-					<div class="accordion-expand"></div>
-				  </div>
-				  <div class="accordion-content">
-					<div class="accordion-text">
-					  <p>You can purchase an unlimited number of bot sessions as packs. You can purchase extra packs at $100 for 1000 sessions. The validity of a pack aligns with your payment cycle. For example: if you choose to pay quarterly, your bot sessions will also expire quarterly.</p>
-
-					</div>
-				  </div>   
-				</div>
-
-
-				  </div>
-				</div> <!-- End row -->
-			  </div>
 			</section>
 			
             <RelatedServices />
