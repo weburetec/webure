7e412f8fc9ea6d7b5d2bbf6f9bbae6131a89ffe5
weburetech <nitin@webure.in>
2021-05-08 15:16:04 +0530

    Update DedicatedServerContent.js

--- src/components/ServiceDetails/DedicatedServerContent.js
+++ src/components/ServiceDetails/DedicatedServerContent.js
@@ -17,16 +17,16 @@ const DedicatedServerContent = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-8 col-md-12">
-						<h2 class="font-size-6 mb-0"><strong>Get The Cheapest Enterprise-Grade Dedicated Servers In The Market</strong></h2>
+						<h2 class="font-size-6 mb-0"><strong>Cheapest Enterprise-Grade Dedicated Servers</strong></h2>
 						<div class="ahl"></div>
-						<span class="font-size-3 text-gray line-height-2">Webure Technologies is a forward thinking and creative Full service agency based in Bengaluru having global office in New York, London, Australia and New Zealand that provides consultancy and have a proven track record in helping small, medium and large businesses achieve high levels of success. Our consulting services can be tailored for your needs, be it large, medium or small campaigns. Webure Technologies are a leading Full service digital transformation agency committed in helping businesses to meet their online potential.</span>
+						<span class="font-size-8 text-gray line-height-2">Webure Technologies is a forward thinking and creative Full service agency based in Bengaluru having global office in New York, London, Australia and New Zealand that provides consultancy and have a proven track record in helping small, medium and large businesses achieve high levels of success. Our consulting services can be tailored for your needs, be it large, medium or small campaigns. Webure Technologies are a leading Full service digital transformation agency committed in helping businesses to meet their online potential.</span>
 						<br/>				
 						<div className="row">
 							<div className="col-lg-4 col-md-12">
 								<h2 class="font-size-8 mb-0"><strong>Get more leads and sales with Digital Marketing</strong></h2>						
 							</div>						
 							<div className="col-lg-8 col-md-12">
-								<span class="font-size-8 mb-0">Do you have an idea for an app and are looking for a trusted app design and development agency in India ? Look no further ! The experienced digital marketing professionals at Webure Technologies work with you to develop a media strategy, conduct media research, create a media plan and ultimately help you to launch and optimize your online digital campaign. Webure Technologies offers complete Digital Marketing package including web design , website development, Internet marketing. Our range of internet marketing services includes search engine optimization (SEO), Google Ads, email marketing solution etc.</span>						
+								<span class="font-size-8 mb-0">Webure Technologies guarantees you the best price in the market. If you find a better offer, contact us and we will match your price and offer 25% additional discount on it or offer a free month upgrade.*.</span>						
 							</div>						
 						</div>
 						<br/><br/>			
