f69a2f2dcbdebcb94e3e6bf9adff453d3b2b656a
weburetech <nitin@webure.in>
2021-05-09 18:16:25 +0530

    Update ContactForm.js

--- src/components/Career/ContactForm.js
+++ src/components/Career/ContactForm.js
@@ -1,96 +1,34 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
 import { useState } from "react";
 import starIcon from '../../assets/images/star-icon.webp'
 import contact from '../../assets/images/contact.webp'
 
-const ContactForm = () => {
-	const [status, setStatus] = useState("Submit");
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setStatus("Sending...");
-		const { name, email, message } = e.target.elements;
-			let details = {
-				name: name.value,
-				email: email.value,
-				message: message.value,
-			};
-			let response = await fetch("http://localhost:5000/contact", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json;charset=utf-8",
-				},
-				body: JSON.stringify(details),
-			});
-		setStatus("Submit");
-		let result = await response.json();
-		alert(result.status);
-	};
-    return (
-        <section className="contact-area pb-100">
-            <div className="container">
-                <div className="section-title">
-                    <h4>Apply Job</h4>
-                </div>
+export default function ContactUs() {
 
-                <div className="row">
-                        <div className="contact-form">
-                            <form id="contactForm">
-                                <div className="row">
-                                    <div className="col-lg-12 col-md-6">
-                                        <div className="form-group">
-                                            <input type="text" name="name" className="form-control" required placeholder="Name" />
-                                        </div>
-                                    </div>
+  function sendEmail(e) {
+    e.preventDefault();
 
-                                    <div className="col-lg-12 col-md-6">
-                                        <div className="form-group">
-                                            <input type="email" name="email" className="form-control" required placeholder="Your email address" />
-                                        </div>
-                                    </div>
+    emailjs.sendForm('service_1j0jnpc', 'template_3wshj9r', e.target, 'user_Mi95JS4Z2zPcTdA39Z0l9')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
 
-                                    <div className="col-lg-12 col-md-12">
-                                        <div className="form-group">
-                                            <input type="text" name="phone_number" className="form-control" required placeholder="Your phone number" />
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-12 col-md-12">
-                                        <div className="form-group">
-                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Objective" />
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-12 col-md-12">
-                                        <div className="form-group">
-                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Experience" />
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-12 col-md-12">
-                                        <div className="form-group">
-                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="- To make money. To kill time. To get popular. Sorry, not interested. Next!  To learn and learn more. To take up a challenging role. To grow with growing organization. Great!!!" />
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-12 col-md-12">
-                                        <div className="form-group">
-                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Write your message..." />
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-12 col-md-12">
-                                        <button type="submit" className="default-btn">
-                                            <i className="flaticon-tick"></i> 
-                                            Send Message <span></span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
 
 export default ContactForm
