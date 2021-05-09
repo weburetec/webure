ebd5e1a580b391e855c2ae7f95b6f09d77c5e916
weburetech <nitin@webure.in>
2021-05-09 19:10:26 +0530

    Update ContactForm.js

--- src/components/Career/ContactForm.js
+++ src/components/Career/ContactForm.js
@@ -1,43 +1,85 @@
-import emailjs from "emailjs-com";
-import React from 'react';
+import React from 'react'
+import emailjs from 'emailjs-com';
 
-export default function ContactUs() {
 
-    function sendEmail(e) {
-        e.preventDefault();
-
-    emailjs.sendForm('gmail', 'youtube_template', e.target, 'user_JABO21I8Gm6sxByJH17Nu')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
-
-    return(
-        <div>
+const ContactForm = () => {
+	
+	function sendEmail(e) {
+		e.preventDefault();
+		emailjs.sendForm('service_1j0jnpc', 'template_3wshj9r', e.target, 'user_Mi95JS4Z2zPcTdA39Z0l9')
+		.then((result) => {
+		console.log(result.text);
+	}, (error) => {
+		console.log(error.text);
+	});
+	}
+	
+    return (
+        <section className="contact-area pb-100">
             <div className="container">
-            <form onSubmit={sendEmail}>
-                    <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                <div className="section-title">
+                    <h4>Apply Job</h4>
+                </div>
+
+                <div className="row">
+                        <div className="contact-form">
+                            <form id="contactForm">
+                                <div className="row">
+                                    <div className="col-lg-12 col-md-6">
+                                        <div className="form-group">
+                                            <input type="text" name="name" className="form-control" required placeholder="Name" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-6">
+                                        <div className="form-group">
+                                            <input type="email" name="email" className="form-control" required placeholder="Your email address" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <input type="text" name="phone_number" className="form-control" required placeholder="Your phone number" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Objective" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Experience" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="- To make money. To kill time. To get popular. Sorry, not interested. Next!  To learn and learn more. To take up a challenging role. To grow with growing organization. Great!!!" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="form-group">
+                                            <textarea name="message" className="form-control" cols="30" rows="6" required placeholder="Write your message..." />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <button type="submit" className="default-btn">
+                                            <i className="flaticon-tick"></i> 
+                                            Send Message <span></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
+
+export default ContactForm
