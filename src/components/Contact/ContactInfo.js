d020f01c8a7046b25c8c483facf18ab9fe4d384f
weburetech <nitin@webure.in>
2021-06-09 01:01:10 +0530

    Update ContactInfo.js

--- src/components/Contact/ContactInfo.js
+++ src/components/Contact/ContactInfo.js
@@ -7,7 +7,7 @@ const ContactInfo = () => {
             <div className="container">
 				<div class="row">
 					<div class="col-lg-4 col-sm-6">
-						<H2><Strong>GET IN TOUCH <mark>WITH US</mark></Strong></H2>
+						<h2><Strong>GET IN TOUCH <mark>WITH US</mark></Strong></h2>
 					</div>
 					<div class="col-lg-8 col-sm-6">
 						<ContactForm />
