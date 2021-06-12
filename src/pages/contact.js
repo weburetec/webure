bb3ab5ea466cb0faee05df7027642f66c61e78b6
weburetech <nitin@webure.in>
2021-06-12 21:39:09 +0530

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,6 @@ import Navbar from "../components/App/Navbar"
 import PageBanner from '../components/Common/PageBanner'
 import Footer from "../components/App/Footer"
 import ContactInfo from '../components/Contact/ContactInfo'
-import ContactForm from '../components/Contact/ContactForm'
 import ContactInfooffice from '../components/Contact/ContactInfooffice'
 const Contact = () => {
     return (
