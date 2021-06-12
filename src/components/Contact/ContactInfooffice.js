51898549eaddf14224a84ef9d54158ccdf65ff6c
weburetech <nitin@webure.in>
2021-06-12 21:57:59 +0530

    Update ContactInfooffice.js

--- src/components/Contact/ContactInfooffice.js
+++ src/components/Contact/ContactInfooffice.js
@@ -69,26 +69,23 @@ const ContactInfooffice = () => {
 					<div class="col-lg-4 col-sm-6">
 						<hr class="new1" />
 						<br />
-						<h2><strong>Help us get in touch <span class="contactopening">with</span><br /> <span class="contactopening">you</span></strong></h2>
+						<h2><strong>Help us get in touch <br /> <span class="contactopening">with you</span></strong></h2>
 					</div>
 					<div class="col-lg-8 col-sm-6">
 						<div class="row">
 							<ContactFormRevised />
 						</div>
 					</div>
-				</div>
-				<br />
-				<br />				
-				
-				
+				</div>			
 				<div class="row">
 					<div class="col-lg-3 col-sm-6">
 						<div class="single-solutions-box">
 							<div class="icon">
 								<img src={contact01} alt="contact01" />
 							</div>
-							<h3><a href="/service-details">New York Office</a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+							<h3><a href="/service-details">New Zealand</a></h3>
+							<p>Level 26, HSBC Tower, 188 Quay Street</p>
+							<p>Auckland, AUK 1010 New Zealand</p>
 						</div>
 					</div>
 					<div class="col-lg-3 col-sm-6">
@@ -96,8 +93,9 @@ const ContactInfooffice = () => {
 							<div class="icon">
 								<img src={contact02} alt="contact02" />
 							</div>
-							<h3><a href="/service-details">New York Office</a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+							<h3><a href="/service-details">New York </a></h3>
+							<p>One World Trade Center, 285 Fulton Street Suite 8500</p>
+							<p>New York City NY 10007, USA</p>
 						</div>
 					</div>
 					<div class="col-lg-3 col-sm-6">
@@ -105,8 +103,9 @@ const ContactInfooffice = () => {
 							<div class="icon">
 								<img src={contact03} alt="contact03" />
 							</div>
-							<h3><a href="/service-details">New York Office</a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+							<h3><a href="/service-details">Canada </a></h3>
+							<p>155-2 King Street West Unit #145 Hamilton</p>
+							<p>ON L8P 4S0 Canada</p>
 						</div>
 					</div>
 					<div class="col-lg-3 col-sm-6">
@@ -114,8 +113,9 @@ const ContactInfooffice = () => {
 							<div class="icon">
 								<img src={contact04} alt="contact04" />
 							</div>
-							<h3><a href="/service-details">New York Office</a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+							<h3><a href="/service-details">India Office</a></h3>
+							<p>#310G, Whitefield, Bangalore</p>
+							<p>560103, India</p>
 						</div>
 					</div>
 				</div>
