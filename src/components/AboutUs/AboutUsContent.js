65a7e6bf2bb1af1602adad9693da97bb8733ee4c
weburetech <nitin@webure.in>
2021-06-16 00:17:46 +0530

    Update AboutUsContent.js

--- src/components/AboutUs/AboutUsContent.js
+++ src/components/AboutUs/AboutUsContent.js
@@ -20,7 +20,7 @@ const AboutUsContent = () => {
 			<div className="container">
                 <div className="row">
                     <div className="col-lg-8 col-md-12">
-						<h2 class="font-size-6 mb-0"><strong>ROI Driven digital marketing agency  in Bengaluru, India</strong></h2>
+						<h2><strong>ROI Driven digital marketing agency  in <span class="contactopening">Bengaluru, India</span></strong></h2>
 						<div class="ahl"></div>
 						<span class="font-size-3 text-gray line-height-2">Webure Technologies is a forward thinking and creative Full service agency based in Bengaluru having global office in New York, London, Australia and New Zealand that provides consultancy and have a proven track record in helping small, medium and large businesses achieve high levels of success. Our consulting services can be tailored for your needs, be it large, medium or small campaigns. Webure Technologies are a leading Full service digital transformation agency committed in helping businesses to meet their online potential.</span>
 						<br/>				
