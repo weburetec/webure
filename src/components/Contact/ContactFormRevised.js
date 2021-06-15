a9e8d278f14add8f20d7adcbc1391074ab3b0a53
weburetech <nitin@webure.in>
2021-06-16 01:43:49 +0530

    Update ContactFormRevised.js

--- src/components/Contact/ContactFormRevised.js
+++ src/components/Contact/ContactFormRevised.js
@@ -11,7 +11,7 @@ import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const ContactFormRevised = () => {
-    const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
+    const paperStyle = { padding: '30px 20px', width: 700, margin: "20px auto" }
     const headerStyle = { margin: 0 }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const marginTop = { marginTop: 5 }
