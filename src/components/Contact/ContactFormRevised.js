9018b5694f4f49da1e3d232f36535995b79904cd
weburetech <nitin@webure.in>
2021-06-17 21:13:46 +0530

    Update ContactFormRevised.js

--- src/components/Contact/ContactFormRevised.js
+++ src/components/Contact/ContactFormRevised.js
@@ -11,82 +11,38 @@ import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const ContactFormRevised = () => {
-    const paperStyle = { padding: '30px 20px', width: 700, margin: "20px auto" }
+    const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
     const headerStyle = { margin: 0 }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const marginTop = { marginTop: 5 }
     return (
-
-		<div class="inner-common-form">
-							
-							<div class="contact-cat">
-								<ul>
-									<li class="active"><a href="#" data-reason="General Inquiries">General Inquiries</a></li>
-									<li class=""><a href="#" data-reason="Request a Callback">Request a Callback</a></li>
-									<li class=""><a href="#" data-reason="Request a Quote">Request a Quote</a></li>
-									<li class=""><a href="#" data-reason="Careers">Careers</a></li>
-								</ul>
-							</div>
-							<form id="contact-form" method="post" enctype="multipart/form-data">
-								<input type="hidden" name="action" value="contact_form_ajax"/>
-								<input type="hidden" name="enquiry_for" id="enquiry_for" value="General Inquiries"/>
-								<input type="hidden" name="page_title" value="Contact"/>
-								<div class="row">									
-									<div class="form-group col-md-12">
-										<input type="text" name="full_name" placeholder="Name*"/>
-									</div>
-
-									<div class="form-group col-md-12">
-										<input type="email" name="email" placeholder="Work Email*"/>
-									</div>
-
-									<div class="form-group col-md-12">
-										<input type="number" name="phone" placeholder="Phone*"/>
-									</div>
-
-									<div class="form-group col-md-12 field-general">
-										<input type="text" name="company" placeholder="Company Website"/>
-									</div>
-
-									<div class="form-group col-md-12 field-general">
-										<input type="text" name="drop_line" placeholder="Drop a Line"/>
-									</div>
-
-									<div class="form-group col-md-12 field-quote" style="display: none;">
-										<input type="text" name="services" placeholder="Services*"/>
-									</div>
-
-									<div class="form-group col-md-12 field-quote" style="display: none;">
-										<input type="text" name="requirement" placeholder="Requirement"/>
-									</div>
-
-									<div class="form-group col-md-12 field-callback" style="display: none;">
-										<label for="callback_timeslots1"> Share 3 Time-Slots, we will email you for confirmation</label>
-  									</div>
-									<div class="form-group col-md-12 field-callback" style="display: none;">
-										<input type="text" name="callback_timeslots1" id="callback_timeslots1" placeholder="Time Date Timezone Slot 1*"/>
-  									</div>
-									<div class="form-group col-md-12 field-callback" style="display: none;">
-										<input type="text" name="callback_timeslots2" id="callback_timeslots2" placeholder="Time Date Timezone Slot 2*"/>
-  									</div>
-									<div class="form-group col-md-12 field-callback" style="display: none;">
-										<input type="text" name="callback_timeslots3" id="callback_timeslots3" placeholder="Time Date Timezone Slot 3 (Optional) "/>
-  									</div>
-
-  									<div class="form-group col-md-12 field-careers" style="display: none;">
-										<input type="text" name="position_apply" placeholder="Position applying for"/>
-									</div>
-
-									<div class="form-group col-md-12 field-careers" style="display: none;">
-										<input type="text" name="how_did_you_hear" placeholder="Where did you heard about us?"/>
-									</div>							
-
-
-
-
-								</div>
-							</form>
-						</div>
+        <Grid>
+            <Paper elevation={20} style={paperStyle}>
+                <Grid align='center'>
+                    <h2 style={headerStyle}>Sign Up</h2>
+                    <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
+                </Grid>
+                <form>
+                    <TextField fullWidth label='Name' placeholder="Enter your name" />
+                    <TextField fullWidth label='Email' placeholder="Enter your email" />
+                    <FormControl component="fieldset" style={marginTop}>
+                        <FormLabel component="legend">Gender</FormLabel>
+                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
+                            <FormControlLabel value="female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="male" control={<Radio />} label="Male" />
+                        </RadioGroup>
+                    </FormControl>
+                    <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
+                    <TextField fullWidth label='Password' placeholder="Enter your password"/>
+                    <TextField fullWidth label='Confirm Password' placeholder="Confirm your password"/>
+                    <FormControlLabel
+                        control={<Checkbox name="checkedA" />}
+                        label="I accept the terms and conditions."
+                    />
+                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
+                </form>
+            </Paper>
+        </Grid>
     )
 }
 
