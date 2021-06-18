1872c02483e681e55cfe1535e986de6952b4b50e
weburetech <nitin@webure.in>
2021-06-19 00:59:48 +0530

    Update ContactInfooffice.js

--- src/components/Contact/ContactInfooffice.js
+++ src/components/Contact/ContactInfooffice.js
@@ -77,164 +77,7 @@ const ContactInfooffice = () => {
 						</div>
 					</div>
 				</div>			
-
-				<div class="row">
-					<div class="locations-main row mainTabing">
-						<div class="map-part col-md-7 col-lg-8">
-							<div class="accMainDiv">
-								<h2 class="resp-accordion minus" role="tab">
-									<span class="resp-arrow"></span>
-									<div class="contact-info">
-									<h4 class="name">India</h4>
-									<div class="address">#310G, Whitefield Post Office Road, Whitefield, Bangalore, Karnataka, India - 560103</div>
-										<div><a href="tel:+12136746665">+1 213 674 6665</a></div>
-									</div>
-									<div class="direction-btn">
-										<button class="cta-button">Get Directions</button>
-									</div>
-								</h2>
-								<div class="accoContain current in">
-									<div class="map-block">
-									
-									</div>
-								</div>
-								<h2 class="resp-accordion" role="tab"><span class="resp-arrow"></span>
-									<div class="contact-info">
-										<h4 class="name">Canada</h4>
-										<div class="address">155-2 King Street West Unit #145 Hamilton, ON L8P 4S0 Canada</div>
-										<div><a href="tel:+610872006665">+61 08 7200 6665</a></div>
-									</div>
-									<div class="direction-btn">
-										<button class="cta-button">Get Directions</button>
-									</div>
-								</h2>
-								<div class="accoContain">
-									<div class="map-block">
-									
-									</div>
-								</div>
-								<h2 class="resp-accordion" role="tab"><span class="resp-arrow"></span>
-									<div class="contact-info">
-										<h4 class="name">New York</h4>
-										<div class="address">One World Trade Center, 285 Fulton Street Suite 8500, New York City NY 10007 U.S.A</div>
-										<div><a href="tel:+31647076508">+31 23 204 9966</a></div>
-									</div>
-									<div class="direction-btn">
-										<button class="cta-button">Get Directions</button>
-									</div>
-								</h2>
-								<div class="accoContain">
-									<div class="map-block">
-									
-									</div>
-								</div>
-								<h2 class="resp-accordion" role="tab"><span class="resp-arrow"></span>
-									<div class="contact-info">
-										<h4 class="name">Australia</h4>
-										<div class="address">Level 27, 101 Collins Street Melbourne Australia</div>
-										<div><a href="tel:+917940324566">+91 79 40324566</a></div>
-									</div>
-									<div class="direction-btn">
-										<button class="cta-button">Get Directions</button>
-									</div>
-								</h2>
-								<div class="accoContain">
-									<div class="map-block">
-									
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="address-part detailTab col-md-5 col-lg-4">
-						<div class="location-box tabing current">
-							<div class="contact-info">
-								<h4 class="name">India</h4>
-								<div class="address">#310G, Whitefield Post Office Road, Whitefield, Bangalore, Karnataka, India - 560103</div>
-								<div><a href="tel:+12136746665">+1 213 674 6665</a></div>
-							</div>
-							<div class="direction-btn">
-								<button class="cta-button">Get Directions</button>
-							</div>
-						</div>
-						<div class="location-box tabing">
-							<div class="contact-info">
-								<h4 class="name">Canada</h4>
-								<div class="address">155-2 King Street West Unit #145 Hamilton, ON L8P 4S0 Canada</div>
-								<div><a href="tel:+610872006665">+61 08 7200 6665</a></div>
-							</div>
-							<div class="direction-btn">
-								<a href="https://goo.gl/maps/Y1JgSndRuWSJbjRR9" target="_blank" class="cta-button">Get Directions</a>
-							</div>
-						</div>
-						<div class="location-box tabing">
-							<div class="contact-info">
-								<h4 class="name">New York</h4>
-								<div class="address">One World Trade Center, 285 Fulton Street Suite 8500, New York City NY 10007 U.S.A</div>
-									<div><a href="tel:+31647076508">+31 23 204 9966</a></div>
-								</div>
-								<div class="direction-btn">
-									<button class="cta-button">Get Directions</button>
-								</div>
-						</div>
-						<div class="location-box tabing">
-							<div class="contact-info">
-								<h4 class="name">Australia</h4>
-								<div class="address">Level 27, 101 Collins Street Melbourne Australia</div>
-								<div><a href="tel:+917940324566">+91 79 40324566</a></div>
-							</div>
-							<div class="direction-btn">
-								<button class="cta-button">Get Directions</button>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="col-lg-3 col-sm-6">
-						<div class="single-solutions-box">
-							<div class="icon">
-								<img src={contact01} alt="contact01" />
-							</div>
-							<h3><a href="/service-details">New Zealand</a></h3>
-							<p>Level 26, HSBC Tower, 188 Quay Street, Auckland, AUK 1010 New Zealand</p>
-							<p></p>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<div class="single-solutions-box">
-							<div class="icon">
-								<img src={contact02} alt="contact02" />
-							</div>
-							<h3><a href="/service-details">New York </a></h3>
-							<p>One World Trade Center, 285 Fulton Street Suite 8500, New York City NY 10007, USA</p>
-							<p></p>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<div class="single-solutions-box">
-							<div class="icon">
-								<img src={contact03} alt="contact03" />
-							</div>
-							<h3><a href="/service-details">Canada </a></h3>
-							<p>155-2 King Street West Unit #145 Hamilton, ON L8P 4S0 Canada</p>
-							<p></p>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<div class="single-solutions-box">
-							<div class="icon">
-								<img src={contact04} alt="contact04" />
-							</div>
-							<h3><a href="/service-details">India Office</a></h3>
-							<p>#310G, Whitefield, Bangalore, 560103, India</p>
-							<p></p>
-						</div>
-					</div>
-				</div>
-			
             </div>
-			
 			
 			
 			
@@ -244,8 +87,8 @@ const ContactInfooffice = () => {
 					<div class="accMainDiv">
 						<h2 class="resp-accordion minus" role="tab"><span class="resp-arrow"></span>
 						<div class="contact-info">
-							<h4 class="name">United States</h4>
-							<div class="address">9655 Granite Ridge Dr Suite 200, CA 92123</div>
+							<h4 class="name">India</h4>
+							<div class="address">#310G, Whitefield Post Office Road, Whiefield, Bangalore, Karnataka 560103, India</div>
 							<div><a href="tel:+12136746665">+1 213 674 6665</a></div>
 						</div>
 						<div class="direction-btn">
@@ -260,52 +103,34 @@ const ContactInfooffice = () => {
 						</div>
 						<h2 class="resp-accordion" role="tab"><span class="resp-arrow"></span>
 						<div class="contact-info">
-							<h4 class="name">Australia</h4>
-							<div class="address">Level 6, 241 Commonwealth Street, Surry Hills, NSW 2010</div>
+							<h4 class="name">Canada</h4>
+							<div class="address">155-2 King Street West Unit #145 Hamilton, ON L8P 4S0 Canada</div>
 							<div><a href="tel:+610872006665">+61 08 7200 6665</a></div>
 						</div>
 						<div class="direction-btn">
 							<a href="https://goo.gl/maps/Y1JgSndRuWSJbjRR9" target="_blank" class="cta-button">Get Directions</a>
 						</div>
-					</h2><div class="accoContain">
-							<div class="map-block">
-						
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.230931967279!2d151.20746351521063!3d-33.883705880652094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12b07d567c3673%3A0x36ee546b38b6542a!2sUplers!5e0!3m2!1sen!2sin!4v1596779639529!5m2!1sen!2sin"></iframe>
-					
-							</div>
-						</div>
+					</h2>
 						<h2 class="resp-accordion" role="tab"><span class="resp-arrow"></span>
 						<div class="contact-info">
-							<h4 class="name">Netherlands</h4>
-							<div class="address">Wilhelminastraat 14, 2011VM, Haarlem, The Netherlands</div>
+							<h4 class="name">New York</h4>
+							<div class="address">One World Trade Center, 285 Fulton Street Suite 8500, New York City NY 10007, USA</div>
 <div><a href="tel:+31647076508">+31 23 204 9966</a></div>
 						</div>
 						<div class="direction-btn">
 							<a href="https://goo.gl/maps/dqQRhH6JDN5SUwG3A" target="_blank" class="cta-button">Get Directions</a>
 						</div>
-					</h2><div class="accoContain">
-							<div class="map-block">
-						
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.4181616698743!2d4.625820215949349!3d52.38096945427555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5ef145bb02ff3%3A0xf434a02f4d865dd8!2sWilhelminastraat%2014%2C%202011%20VM%20Haarlem%2C%20Netherlands!5e0!3m2!1sen!2sin!4v1568959247238!5m2!1sen!2sin"></iframe>
-				
-							</div>
-						</div>
+					</h2>
 						<h2 class="resp-accordion" role="tab"><span class="resp-arrow"></span>
 						<div class="contact-info">
-							<h4 class="name">India</h4>
-							<div class="address">Uplers House, Next to Kalasagar Mall, Sattadhar Cross Roads, Ahmedabad - 380061</div>
+							<h4 class="name">New Zealand</h4>
+							<div class="address">Level 26, HSBC Tower, 188 Quay Street, Auckland, AUK 1010 New Zealand</div>
 							<div><a href="tel:+917940324566">+91 79 40324566</a></div>
 						</div>
 						<div class="direction-btn">
 							<a href="https://goo.gl/maps/B4tJbY7KLmuMYfW88" target="_blank" class="cta-button">Get Directions</a>
 						</div>
-					</h2><div class="accoContain">
-							<div class="map-block">
-
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.861094136046!2d72.52956941496858!3d23.065553584931333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b4e7bc65c17%3A0x8881db708141e320!2sUplers!5e0!3m2!1sen!2sin!4v1596779835557!5m2!1sen!2sin"></iframe>
-
-							</div>
-						</div>
+					</h2>
 					</div>
 					
 				</div>
