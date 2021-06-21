c857ff8f95221fc47c93d8a17602f45fbb8b26ab
weburetech <nitin@webure.in>
2021-06-21 19:59:03 +0530

    Updates

--- src/components/AboutUs/AboutUsContent.js
+++ src/components/AboutUs/AboutUsContent.js
@@ -26,7 +26,7 @@ const AboutUsContent = () => {
 						<br />
 						<h2><strong>About <span class="contactopening">Webure</span></strong></h2>
 						<h4 class="font-size-3 text-gray line-height-2">Webure Technologies is a forward thinking and creative Full service agency based in Bengaluru having global office in New York, London, Australia and New Zealand that provides consultancy and have a proven track record in helping small, medium and large businesses achieve high levels of success. Our consulting services can be tailored for your needs, be it large, medium or small campaigns. Webure Technologies are a leading Full service digital transformation agency committed in helping businesses to meet their online potential.</h4>
-						<h4 class="font-size-3 text-gray line-height-2">We are a Digital Marketing and Technology Agency in India that provide complete end-to-end digital media solutions with higher scope on Brand Strategy, Creating uncontested market space and Analytics. <br />Our R&D team are in the process of creating breakthrough technologies to make advertising more engaging and converting for businesses.</h4>						
+						<h4 class="font-size-3 text-gray line-height-2">We are a Digital Marketing and Technology Agency in India that provide complete end-to-end digital media solutions with higher scope on Brand Strategy, Creating uncontested market space and Analytics. <br />Our R&D team are in the process of creating breakthrough technologies to make advertising more engaging and converting for businesses.<br /><br /><br /></h4>						
 						<div className="events-details-image aa">
 							<UpcomingEventTimer />
 						</div>
