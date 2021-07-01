5773aef5476892746a013ff56be158398cb2b687
weburetech <nitin@webure.in>
2021-07-01 23:46:06 +0530

    Update CareerDetailsContent.js

--- src/components/Career/CareerDetailsContent.js
+++ src/components/Career/CareerDetailsContent.js
@@ -129,31 +129,34 @@ const CareerDetailsContent = () => {
 							</ul>
 						</div>
 						<br />
-							<form>  
+						<form>  
 							<div class="row">
-							  <div class="col-12 mb-2 pb-1">         
-								  <input type="text" placeholder='Full Name'/>                  
-							  </div>
-							  <div class="col-12 mb-2 pb-1">                      
-								  <input type="email" placeholder='Email' />                 
-							  </div>
-							  <div class="col-12 mb-2 pb-1">                    
-								  <input type="tel" placeholder="Phone" />                      
-							  </div>
-
-							  <div class="col-12 mb-2 pb-1">          
-								  <input type="text" placeholder="Position for applying"  />
-							  </div>
-
-							  <div class="col-12 mb-1 pb-1">
-								  <input type="text" multiline={true} numberOfLines={4} placeholder="Objective - To make money. To kill time. To get popular. Sorry, not interested. Next!  To learn and learn more. To take up a challenging role. To grow with growing organization. Great!" />          
-									      <Dropdown formLabel="Choose a service" buttonText="Send form" onChange={handleSelect} action="/">
-											<Option selected value="Click to see options" />
-											<Option value="Option 1" />
-											<Option value="Option 2" />
-											<Option value="Option 3" />
-											</Dropdown>
-							  </div> 
+								<div class="col-12 mb-2 pb-1">         
+									<input type="text" placeholder='Full Name'/>                  
+								</div>
+								<div class="col-12 mb-2 pb-1">                      
+									<input type="email" placeholder='Email' />                 
+								</div>
+								<div class="col-12 mb-2 pb-1">                    
+									<input type="tel" placeholder="Phone" />                      
+								</div>
+								<div class="col-12 mb-2 pb-1">          
+									<input type="text" placeholder="Position for applying"  />
+								</div>
+								<div class="col-12 mb-1 pb-1">
+									<input type="text" multiline={true} numberOfLines={4} placeholder="Objective - To make money. To kill time. To get popular. Sorry, not interested. Next!  To learn and learn more. To take up a challenging role. To grow with growing organization. Great!" />          
+<DropdownButton
+  menuAlign="right"
+  title="Dropdown right"
+  id="dropdown-menu-align-right"
+>
+  <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+  <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+  <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+  <Dropdown.Divider />
+  <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+</DropdownButton>
+								</div> 
 
 							  <div class="col-12 mb-1 pb-1">
 								  <input type="text" placeholder="Experience - Experienced and all set to march a different beat? Yes please! - No or little experience, but game for trying out new things and ready for an adventurous trip? HOP IN!" />          
