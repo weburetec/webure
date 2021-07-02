0ea7e174749316c0f4bc76ecea98c64b6ca49c3e
weburetech <nitin@webure.in>
2021-07-02 23:14:31 +0530

    Update ISMS-policy.js

--- src/pages/ISMS-policy.js
+++ src/pages/ISMS-policy.js
@@ -10,20 +10,27 @@ const Services = () => {
     return (
 	    <Layout>
             <Navbar />
-			<section className="events-details-area pb-100">
-				<article id="post-14808" class="post-14808 page type-page status-publish hentry">
-					<header class="entry-header">
-						<h1 class="entry-title">ISMS Policy</h1>
-					</header>
-					<div class="entry-content">
-						<p>At Webure Technologies, Information Security Management System is voluntary effort and we are committed to identify, evaluate and access the risk for all critical information covering all business processes with consideration of statutory &amp; regulatory requirements and prepare and follow the risk treatment plan to meet the information security management requirements.</p>
-						<p>We are committed to implement the dynamic ISMS with the aim of continual improvement. We shall ensure business continuity and safeguard the confidentiality, integrity and availability of people, process and technology of the business and its customers.</p>
-						<p>We shall continuously provide training and awareness improve the Information Security Management System to reduce business risks and management is committed to provide all resources to protect customer information and safeguard information asset. We are committed to identify and comply all legal / regulatory and contractual obligation related to ISMS.</p>
-						<p>&nbsp;</p>
+			<br />
+			<br />
+			<div class="container">
+				<div class="full-width">						
+					<div class="row">
+						<div class="col-sm-12">
+							<article id="post-14808" class="post-14808 page type-page status-publish hentry">
+								<header class="entry-header allot-position sticky-ready">
+									<h1 class="entry-title">ISMS Policy</h1>
+								</header>
+								<div class="entry-content">
+									<p>At Webure Technologies OPC Private Limited, Information Security Management System is voluntary effort and we are committed to identify, evaluate and access the risk for all critical information covering all business processes with consideration of statutory &amp; regulatory requirements and prepare and follow the risk treatment plan to meet the information security management requirements.</p>
+									<p>We are committed to implement the dynamic ISMS with the aim of continual improvement. We shall ensure business continuity and safeguard the confidentiality, integrity and availability of people, process and technology of the business and its customers.</p>
+									<p>We shall continuously provide training and awareness improve the Information Security Management System to reduce business risks and management is committed to provide all resources to protect customer information and safeguard information asset. We are committed to identify and comply all legal / regulatory and contractual obligation related to ISMS.</p>
+									<p>&nbsp;</p>
+								</div>
+							</article>
 						</div>
-						<Footer />
-				</article>
-			</section>
+					</div>						
+				</div>
+			</div>
             <Footer />
         </Layout>
     );
