137874acc470316da4874d2e99db04ecec218a94
weburetech <nitin@webure.in>
2021-07-04 01:53:05 +0530

    Update ContactInfo.js

--- src/components/Contact/ContactInfo.js
+++ src/components/Contact/ContactInfo.js
@@ -19,6 +19,16 @@ const ContactInfo = () => {
 							<span>will contact you shortly.</span>
 						</h5>
 						<p class="queris_hr wow fadeInDown"><strong>For Enquiries &amp; Sales :</strong><br />
+						<div class="row">
+							<div class="col-6 col-md-6">
+								<h4 class="font-size-3 text-gray line-height-2"><strong>Contact [India]</strong></h4>
+							</div>
+							<div class="col-6 col-md-6">
+								<div className=" shadow single-career-box">
+									<h4 class="font-size-3 text-gray line-height-2 color1">+91 9886528400</h4>
+								</div>
+							</div>
+						</div>
 						Contact [India] : +91 – 9886528400<br />Contact [New Zealand] : +64 9 889 3406<br />Contact [New York] : +1 917 675 3086</p>
 						<div class="row">						
 													
