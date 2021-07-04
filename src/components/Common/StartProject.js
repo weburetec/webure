52287868d775e050a5fb24210c5d713705be2fc4
weburetech <nitin@webure.in>
2021-07-04 19:54:25 +0530

    Update StartProject.js

--- src/components/Common/StartProject.js
+++ src/components/Common/StartProject.js
@@ -17,9 +17,9 @@ const StartProject = () => {
 
                     <div className="col-lg-6 col-md-12">
                         <div className="project-start-content">
-							<h2><strong>We would like to start<span class="contactopening">your project with us</span></strong></h2>
+							<h2 class="gapweb44"><strong>We would like to start<span class="contactopening">your project with us</span></strong></h2>
                             <h2>We Like to Start Your Project With Us</h2>
-                            <h4 class="font-size-3 text-gray line-height-2">Our digital marketing team is always ready to help. Give us a call, drop us an email, or just fill in the form below to instantly schedule a call with us.</h4>
+                            <h4 class="gapweb44 text-gray line-height-2">Our digital marketing team is always ready to help. Give us a call, drop us an email, or just fill in the form below to instantly schedule a call with us.</h4>
 
                             <Link to="/contact" className="default-btn">
                                 <i className="flaticon-web"></i> 
