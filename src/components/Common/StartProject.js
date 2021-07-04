09f38dc98451d5f13fd557080b5e7812b3c93b0f
weburetech <nitin@webure.in>
2021-07-04 21:06:57 +0530

    Update StartProject.js

--- src/components/Common/StartProject.js
+++ src/components/Common/StartProject.js
@@ -18,7 +18,6 @@ const StartProject = () => {
                     <div className="col-lg-6 col-md-12">
                         <div className="gappweb44 project-start-content">
 							<h4 class="gapweb44"><strong>We would like to start<span class="contactopening">your project with us</span></strong></h4>
-                            <h4 class="gapweb44 text-gray line-height-2">We Like to Start Your Project With Us</h4>
                             <h4 class="gapweb44 text-gray line-height-2">Our digital marketing team is always ready to help. Give us a call, drop us an email, or just fill in the form below to instantly schedule a call with us.</h4>
 
 							<div class="col-12 col-md-12 footer-right">
@@ -28,7 +27,7 @@ const StartProject = () => {
 												<div class="input">
 													<input type="hidden" name="action" value="newsletter_subscription" />
 													<input type="hidden" name="page_title" value="Contact" />
-													<input type="text" placeholder="Email to which we can reach you*" name="email" />
+													<input type="text" placeholder="Email to reach you*" name="email" />
 												</div>
 			
 												<button class=" mt-2 mb-2 btn btn-warning" type="submit" name="subscribeBtn">subscribe</button>
