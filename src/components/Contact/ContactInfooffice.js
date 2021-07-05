330053749c8ed84906870dad72b61cd09c58a8f5
weburetech <nitin@webure.in>
2021-07-06 00:25:17 +0530

    Update ContactInfooffice.js

--- src/components/Contact/ContactInfooffice.js
+++ src/components/Contact/ContactInfooffice.js
@@ -8,68 +8,7 @@ import map from '../../assets/images/events/map-1024x655.webp'
 
 const ContactInfooffice = () => {
     return (
-        <div className="contact-info-area pb-70">
-            <div className="container">
-				<div class="row">
-					<div class="col-lg-4 col-sm-6">
-						<hr class="new1" />
-						<br />
-						<h2><strong>We have a list of<br /> <br /><span class="contactopening">openings</span></strong></h2>
-					</div>
-					<div class="col-lg-8 col-sm-6 mt-4">
-						<div class="row">
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">UI/UX Designer</p>
-								</div>					
-							</div>
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">Flutter Developer</p>
-								</div>					
-							</div>
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">People Manager</p>
-								</div>					
-							</div>
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">Business Manager</p>
-								</div>					
-							</div>	
-						</div>
-						<div class="row">
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">UI/UX Designer</p>
-								</div>					
-							</div>
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">Flutter Developer</p>
-								</div>					
-							</div>
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">People Manager</p>
-								</div>					
-							</div>
-							<div class="col-lg-3 col-sm-6">
-								<div className="single-career-box">
-									<p class="color1">Business Manager</p>
-								</div>					
-							</div>	
-						</div>
-					</div>
-				</div>
-				<br />
-				<br />
-				
-	            </div>
-			
-			
-			
+        <div className="contact-info-area pb-70">		
 			<section class="locations-main">
 					<div class="row mainTabing">
 						<div class="map-part col-12 col-md-8">
@@ -124,7 +63,7 @@ const ContactInfooffice = () => {
 							</div>
 						</div>
 					</div>
-				</section>			
+			</section>			
         </div>
     )
 }
