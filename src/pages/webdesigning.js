fcabaea8fa39a76272bf43c24f45061f0ae62cd6
weburetech <nitin@webure.in>
2021-07-13 20:10:12 +0530

    Updates

---
+++ src/pages/webdesigning.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from "../components/App/Layout"
+import Navbar from "../components/App/Navbar"
+import PageBanner from '../components/Common/PageBanner'
+import Footer from "../components/App/Footer"
+import ServicesOne from '../components/Services/ServicesOne'
+import StartProject from '../components/Common/StartProject'
+
+const Services = () => {
+    return (
+        <Layout>
+            <Navbar />
+            <ServicesTwo />
+            <StartProject />
+            <Footer />
+        </Layout>
+    );
+}
+
+export default Services
