23c8f9d7929aec8c8704d5ee6a7095243fff0ed4
weburetec <58927451+weburetec@users.noreply.github.com>
2023-01-25 18:25:12 +0530

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,13 +1,4 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-68137489-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-68137489-1');
-</script>
-<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-MBMB44M8GD"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
